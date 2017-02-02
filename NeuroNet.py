bd4c49c64e8e034cf6bf83fba4be6e9bd9f270c8
Sahand Hariri <sahand@meb-0220c-01.mechse.illinois.edu>
2017-02-02 12:42:30 -0600

    Synaptic connection works

--- NeuroNet.py
+++ NeuroNet.py
@@ -3,6 +3,7 @@
 import warnings
 import numpy as np
 from GeneralModel import *
+import random
 
 class Neuron(GeneralModel):
     def __init__(self, ID, synapses=[], tstart=0, tend=200, dt=0.1, **params):
@@ -10,8 +11,10 @@ class Neuron(GeneralModel):
         self._ID       = ID
         self._V        = 0
         self._w        = 0
+        self._Input    = params["I"]
         self.Initialize([self._V,self._w])
         self._Synapses = synapses
+        self._II       = np.zeros_like(self._Time) #important it comes afeter initialization.
 
     def Flow(self, t, x, params):
         I   = params["I"]
@@ -19,26 +22,38 @@ class Neuron(GeneralModel):
         b   = params["b"]
         tau = params["tau"]
 
+        #wsign = np.sign(x[1])
+        #absw  = abs(x[1])
+        #V, w = x[0], wsign*min(absw,2)
+
         V, w = x[0], x[1]
 
-        dV = V - V**3/3 - w + I
+        insign = np.sign(self._Input)
+        absin  = abs(self._Input)
+        self._Input = insign*min(absin,2)
+
+        dV = V - V**3/3 - w + self._Input
         dw = (V + a - b*w)/tau
 
         return np.array([dV, dw])
 
     def UpdateSynapses(self):
-        for s in self.Synapses:
-            s.ParentNeuron._w + s._ChildNeuron._w
+        for s in self._Synapses:
+            s._ChildNeuron._Input += 0.01*(s._ParentNeuron._w-0.5)
 
     def Update(self,i):
-        self._GM.UpdateRK(i)
-        self._Time = self._GM._t
-        self._V = self._GM._x[0]
-        self._w = self._GM._x[1]
+        self.StoreInputHistory(i)
+        self.UpdateSynapses()
+        self.UpdateRK(i)
+        self._V = self._x[0]
+        self._w = self._x[1]
+
+    def StoreInputHistory(self,i):
+        self._II[i] = self._Input
 
 class Synapse:
     def __init__(self, ID, upstream, downstream):
-        self._ID = ID
+        self._ID   = ID
         self.SetParentNeuron(upstream)
         self.SetChildNeuron(downstream)
 
@@ -47,13 +62,17 @@ class Synapse:
     def SetChildNeuron(self, downstream):
         self._ChildNeuron = downstream
 
+
 class Brain:
     def __init__(self, neurons=[], dt = 0.1, tend=200):
-        self._Neurons = neurons
-        self._dt      = dt
-        self._Tend    = tend
-        self._TLen    = int(tend/dt)
-
+        self._Neurons      = neurons
+        self._Synapses     = {}
+        self._SynapseCount = 0
+        self._t            = 0
+        self._dt           = dt
+        self._Tend         = tend
+        self._TLen         = int(tend/dt)
+        self._prob         = 1000*[False]+[True]
         if len(self._Neurons)==0:
             self._Neurons.append(Neuron(1,a=0.8,b=0.7,tau=12.5,I=0.5))
 
@@ -62,9 +81,21 @@ class Brain:
             self.Update(i)
 
     def Update(self,i):
+        self._t += self._dt
         for n in self._Neurons:
-            n.UpdateRK(i)
+            self.SynapticActivity(n)
+            n.Update(i)
 
+    def SynapticActivity(self,neuron):
+        if random.choice(self._prob):
+            dneuron = random.choice(self._Neurons)
+            if neuron._ID != dneuron._ID:
+                self._SynapseCount += 1
+                s = Synapse(self._SynapseCount,neuron,dneuron)
+                print('A synampse just formed between Neuron {} and Neuron {} at time = {}'.format(neuron._ID,dneuron._ID,self._t))
+                neuron._Synapses.append(s)
+                #dneuron._Synapses[self._SynapseCount]="In"
+                self._Synapses[self._SynapseCount]=s
 
 if __name__ == '__main__':
     b = Brain()

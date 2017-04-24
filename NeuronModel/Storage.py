71b54330033c5f1947c339aa835f358a3038b7f6
Sahand Hariri <sahand@Sahands-MacBook-Pro.local>
2017-04-24 09:54:30 -0500

    JSON file of parameters. Can construct storage object from file.

--- NeuronModel/Storage.py
+++ NeuronModel/Storage.py
@@ -1,12 +1,37 @@
-
+import json
 
 class Storage:
-    def __init__(self, brain, DataFolder, NeuronsPerFile):
-        self._Brain         = brain
-        self._DataFolder    = DataFolder
-        self._NumberOfFiles = max(1,int(self._Brain._NumberOfNeurons/NeuronsPerFile))
-        self._FileNames     = {}
+    def __init__(self, DataFolder, NeuronsPerFile, brain=None, NumberOfFiles=None, NumberOfNeurons=None):
+        self._Brain          = brain
+        self._DataFolder     = DataFolder
+        self.GetParams(NumberOfNeurons,NumberOfFiles,NeuronsPerFile)
+        self._FileNames={}
+        self._FullData=[]
         self.AssembleFileNames()
+
+
+    @classmethod
+    def FromFile(cls, name):
+        with open(name) as data_file:
+            data = json.load(data_file)
+        NeuronsPerFile  = data["Neurons Per File"]
+        DataFolder      = data["Data Folder"]
+        NumberOfFiles   = data["Number of Files"]
+        NumberOfNeurons = data["Number of Neurons"]
+
+        return cls(DataFolder, NeuronsPerFile=NeuronsPerFile, NumberOfFiles=NumberOfFiles, NumberOfNeurons=NumberOfNeurons)
+
+    def GetParams(self,NumberOfNeurons,NumberOfFiles,NeuronsPerFile):
+        if self._Brain == None:
+            self._NeuronsPerFile = NeuronsPerFile
+            self._NumberOfNeurons = NumberOfNeurons
+            self._NumberOfFiles   = NumberOfFiles
+            self._Parameters      = {}
+        else:
+            self._NeuronsPerFile  = NeuronsPerFile
+            self._NumberOfNeurons = self._Brain._NumberOfNeurons
+            self._NumberOfFiles   = max(1,int(self._Brain._NumberOfNeurons/NeuronsPerFile))
+            self._Parameters      = self._Brain._Params
 
     def AssembleFileNames(self):
         for i in range(self._NumberOfFiles):
@@ -15,7 +40,7 @@ class Storage:
 
     def ConstructLine(self,fileID):
         l = "{:.3f}".format(self._Brain._t) + ', '
-        for i in range(self._Brain._NumberOfNeurons):
+        for i in range(fileID*self._NeuronsPerFile,min((fileID+1)*self._NeuronsPerFile,self._Brain._NumberOfNeurons)):
                 l += "{:.3f}".format(self._Brain._X[i]) + ', '
         l += '\n'
         return l
@@ -25,3 +50,14 @@ class Storage:
             l = self.ConstructLine(fileID)
             with open(self._FileNames[fileID],"a") as f: #in write mode
                 f.write(l)
+
+    def WriteParameters(self):
+        with open(self._DataFolder+"/Parameters.json", 'w') as f:
+            self._Parameters["Neurons Per File"] = self._NeuronsPerFile
+            self._Parameters["Number of Files"]  = self._NumberOfFiles
+            self._Parameters["Data Folder"] = self._DataFolder
+            self._Parameters["Number of Neurons"] = self._Brain._NumberOfNeurons
+            json.dump(self._Brain._Params, f, indent=4, separators=(',', ': '))
+
+    def ReadData(self):
+        for file in self._FileNames[]
--- NeuronModel/run.py
+++ NeuronModel/run.py
@@ -1,19 +1,23 @@
 from NeuronModel import *
 from Storage import *
 
+fromFile = True
 
-C,gL,gCa,gK,VL,VCa,VK,V1,V2,V3,V4,phi=20,2.0,4.4,8,-60,120,-84,-1.2,18.0,2.0,30.0,0.04
-C_v,gL_v,gCa_v,gK_v,VL_v,VCa_v,VK_v,V1_v,V2_v,V3_v,V4_v,phi_v=1,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01
+if fromFile:
+    storage = Storage.FromFile('/Users/sahand/Research/NeuroNet/Data/Parameters.json')
+else:
+    C,gL,gCa,gK,VL,VCa,VK,V1,V2,V3,V4,phi=20,2.0,4.4,8,-60,120,-84,-1.2,18.0,2.0,30.0,0.04
+    C_v,gL_v,gCa_v,gK_v,VL_v,VCa_v,VK_v,V1_v,V2_v,V3_v,V4_v,phi_v=1,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01
 
-connectionscale = 40
-N = 10
+    connectionscale = 40
+    N = 10
 
-brain = NeuronModel(N=15, tend=10, I=0, connectionscale=connectionscale, synapselimit=50, synapsestrengthlimit=60,
-                    C=C,gL=gL,gCa=gCa,gK=gK,VL=VL,VCa=VCa,VK=VK,V1=V1,V2=V2,V3=V3,V4=V4,phi=phi,
-                    I_v=0.1,C_v=C_v,gL_v=gL_v,gCa_v=gCa_v,gK_v=gK_v,VL_v=VL_v,VCa_v=VCa_v,VK_v=VK_v,V1_v=V1_v,V2_v=V2_v,V3_v=V3_v,V4_v=V4_v,phi_v=phi_v)
+    brain = NeuronModel(N=15, tend=10, I=0, connectionscale=connectionscale, synapselimit=50, synapsestrengthlimit=60,
+                        C=C,gL=gL,gCa=gCa,gK=gK,VL=VL,VCa=VCa,VK=VK,V1=V1,V2=V2,V3=V3,V4=V4,phi=phi,
+                        I_v=0.1,C_v=C_v,gL_v=gL_v,gCa_v=gCa_v,gK_v=gK_v,VL_v=VL_v,VCa_v=VCa_v,VK_v=VK_v,V1_v=V1_v,V2_v=V2_v,V3_v=V3_v,V4_v=V4_v,phi_v=phi_v)
 
-storage =  Storage(brain, '/u/eot/hariria2/scratch/DataStoreTest', 5)
-brain.SetStorage(storage)
-brain.DevelopNetwork(120)
-brain.Simulate(source='script')
-
+    storage =  Storage('/Users/sahand/Research/NeuroNet/Data', 15,brain=brain)
+    storage.WriteParameters()
+    brain.SetStorage(storage)
+    brain.DevelopNetwork(120)
+    brain.Simulate(source='script')

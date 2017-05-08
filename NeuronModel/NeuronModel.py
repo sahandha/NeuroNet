ff3d59f2ba0281388ee3ab0fbaacdc462a70470b
Sahand Hariri <sahand@Sahands-MacBook-Pro.local>
2017-05-07 19:13:44 -0500

    runs in parallel, but results aren't right yet

--- NeuronModel/NeuronModel.py
+++ NeuronModel/NeuronModel.py
@@ -1,6 +1,9 @@
-#! python3
+#!/usr/bin/env python
+#######! python3
 import numpy as np
 import itertools as it
+from mpi4py import MPI
+import copy
 
 class NeuronModel():
 
@@ -12,6 +15,8 @@ class NeuronModel():
         self._Time            = np.arange(self._tstart,self._tend,self._dt)
         self._X               = np.array([])
         self._dX              = np.array([])
+        self._Xp              = np.array([])
+        self._dXp             = np.array([])
         self._NumberOfNeurons = N
         self._NoiseMean       = 0
         self._NoiseSTD        = 0.03
@@ -30,6 +35,8 @@ class NeuronModel():
         self.Initialize()
         self.ComputeDistances() #This will result in self_DelayIndx and self._Distance
         # self.DevelopNetwork() #This will result in self._EdgeWieghts = {(n1,n2):w,...}
+        self._Comm  = MPI.COMM_WORLD
+        self._PSize = MPI.COMM_WORLD.size
 
     def SetStorage(self,s):
         self._Storage = s
@@ -78,6 +85,7 @@ class NeuronModel():
     def DevelopNetwork(self,n,source='Jupyter'):
         x = 0;
         self._NetworkDevel = n
+        print("==========> Developing Netowrk <============...hang on...")
         if source=='Jupyter':
             for t in range(n):
                 for key in self._SynapseWeight.keys():
@@ -100,10 +108,16 @@ class NeuronModel():
                         if self._SynapseCount[key[0]] < self._SynapseLimit and self._SynapseCount[key[1]] < self._SynapseLimit:
                             self._SynapseCount[key[0]]+=1
                             self._SynapseCount[key[1]]+=1
+        print("============> Done <=============")
 
     def Initialize(self):
-        self._X    = np.concatenate((np.random.normal(-40,1,size=self._NumberOfNeurons), np.zeros(self._NumberOfNeurons)))
+        self._V    = np.random.normal(-40,1,size=self._NumberOfNeurons)
+        self._N    = np.zeros(self._NumberOfNeurons)
+        self._X    = np.concatenate((self._V, self._N))
+        self._dV   = np.zeros_like(self._V)
+        self._dN   = np.zeros_like(self._N)
         self._dX   = np.zeros_like(self._X)
+
         self._Time = np.arange(self._tstart,self._tend,self._dt)
         self._dim  = len(self._X)
         self._VV   = np.zeros((len(self._Time),self._NumberOfNeurons))
@@ -114,61 +128,86 @@ class NeuronModel():
         self._DelayIndx = np.zeros(self._NumberOfNeurons,dtype=np.int8)
         self._Input = np.zeros(self._NumberOfNeurons)
 
+        self.SplitData()
+
+    def SplitData(self):
+        cs = MPI.COMM_WORLD.size
+        s  = int(self._NumberOfNeurons/cs)
+        r = MPI.COMM_WORLD.rank
+        self._V   = self._X[:self._NumberOfNeurons]
+        self._N   = self._X[self._NumberOfNeurons:]
+        self._Vp  = copy.copy(self._V[s*r:s*(r+1)])
+        self._Np  = copy.copy(self._N[s*r:s*(r+1)])
+        self._dVp = copy.copy(self._dV[s*r:s*(r+1)])
+        self._dNp = copy.copy(self._dN[s*r:s*(r+1)])
+        self._Xp  = np.concatenate((self._Vp, self._Np))
+        self._dXp = np.concatenate((self._dVp, self._dNp))
+        self._Ip  = copy.copy(self._I[s*r:s*(r+1)])
+        self._Inputp = copy.copy(self._Input[s*r:s*(r+1)])
+
     def SetParameters(self):
         params = self._Params
-        self._I   = np.random.normal(params["I"  ],params["I_v"  ],size=self._NumberOfNeurons)
-        self._C   = np.random.normal(params["C"  ],params["C_v"  ],size=self._NumberOfNeurons)
-        self._gCa = np.random.normal(params["gCa"],params["gCa_v"],size=self._NumberOfNeurons)
-        self._VCa = np.random.normal(params["VCa"],params["VCa_v"],size=self._NumberOfNeurons)
-        self._gK  = np.random.normal(params["gK" ],params["gK_v" ],size=self._NumberOfNeurons)
-        self._VK  = np.random.normal(params["VK" ],params["VK_v" ],size=self._NumberOfNeurons)
-        self._gL  = np.random.normal(params["gL" ],params["gL_v" ],size=self._NumberOfNeurons)
-        self._VL  = np.random.normal(params["VL" ],params["VL_v" ],size=self._NumberOfNeurons)
-        self._phi = np.random.normal(params["phi"],params["phi_v"],size=self._NumberOfNeurons)
-        self._V1  = np.random.normal(params["V1" ],params["V1_v" ],size=self._NumberOfNeurons)
-        self._V2  = np.random.normal(params["V2" ],params["V2_v" ],size=self._NumberOfNeurons)
-        self._V3  = np.random.normal(params["V3" ],params["V3_v" ],size=self._NumberOfNeurons)
-        self._V4  = np.random.normal(params["V4" ],params["V4_v" ],size=self._NumberOfNeurons)
+        cs = MPI.COMM_WORLD.size
+        s  = int(self._NumberOfNeurons/cs)
+        self._I   = np.random.normal(params["I"  ],params["I_v"  ],size=s)
+        self._C   = np.random.normal(params["C"  ],params["C_v"  ],size=s)
+        self._gCa = np.random.normal(params["gCa"],params["gCa_v"],size=s)
+        self._VCa = np.random.normal(params["VCa"],params["VCa_v"],size=s)
+        self._gK  = np.random.normal(params["gK" ],params["gK_v" ],size=s)
+        self._VK  = np.random.normal(params["VK" ],params["VK_v" ],size=s)
+        self._gL  = np.random.normal(params["gL" ],params["gL_v" ],size=s)
+        self._VL  = np.random.normal(params["VL" ],params["VL_v" ],size=s)
+        self._phi = np.random.normal(params["phi"],params["phi_v"],size=s)
+        self._V1  = np.random.normal(params["V1" ],params["V1_v" ],size=s)
+        self._V2  = np.random.normal(params["V2" ],params["V2_v" ],size=s)
+        self._V3  = np.random.normal(params["V3" ],params["V3_v" ],size=s)
+        self._V4  = np.random.normal(params["V4" ],params["V4_v" ],size=s)
 
-    def updateSynapses(self,indx):
+    def UpdateSynapses(self,indx):
+        cs = MPI.COMM_WORLD.size
+        s  = int(self._NumberOfNeurons/cs)
+        r = MPI.COMM_WORLD.rank
         EffectiveIndx = indx + self._DelayIndx;
-
-        self._Input = np.zeros(self._NumberOfNeurons)
-        for i in range(self._NumberOfNeurons):
+        self._Inputp = np.zeros_like(self._Vp)
+        for i in range(s):
             input = self._VV[EffectiveIndx,np.arange(self._NumberOfNeurons)]
-            weights = np.array([self._SynapseWeight[(n,i)] for n in range(self._NumberOfNeurons)])
-            self._Input[i] = sum(1/self._SynapseLimit*weights*self._CellType*1/(1+np.exp(-input)))
-        self._Input[EffectiveIndx<0] = 0
+            weights = np.array([self._SynapseWeight[(n,r*(i+1))] for n in range(self._NumberOfNeurons)])
+            self._Inputp[i] = sum(1/self._SynapseLimit*weights*self._CellType*1/(1+np.exp(-input)))
+        self._Inputp[EffectiveIndx<0] = 0
 
     def MLFlow(self, t, x):
 
-        V = self._X[:self._NumberOfNeurons]
-        N = self._X[self._NumberOfNeurons:]
+        V = self._Vp
+        N = self._Np
 
         Mss = 0.5*(1+np.tanh((V-self._V1)/self._V2))
         Nss = 0.5*(1+np.tanh((V-self._V3)/self._V4))
         Tau = 1/(self._phi*np.cosh((V-self._V3)/self._V4/2))
 
-        dV = (self._I - self._gL*(V-self._VL) - self._gCa*Mss*(V-self._VCa) - self._gK*N*(V-self._VK))/self._C + self._Input
-        dN = (Nss - N)/Tau
+        self._dVp = (self._I - self._gL*(V-self._VL) - self._gCa*Mss*(V-self._VCa) - self._gK*N*(V-self._VK))/self._C + self._Inputp
+        self._dNp = (Nss - N)/Tau
 
-        return np.concatenate((dV, dN))
+        return np.concatenate((self._dVp, self._dNp))
 
     def UpdateRK(self,ii):
-        #self._XX[ii,:] = self._X;
-        #self._dXX[ii,:] = self._dX;
-        k1 = self.MLFlow(self._t, self._X)
-        k2 = self.MLFlow(self._t+self._dt/2, self._X+k1*self._dt/2)
-        k3 = self.MLFlow(self._t+self._dt/2, self._X+k2*self._dt/2)
-        k4 = self.MLFlow(self._t+self._dt  , self._X+k2*self._dt  )
-        self._X = self._X + (k1 + 2*k2 + 2*k3 + k4)*self._dt/6
+        cs = MPI.COMM_WORLD.size
+        s  = int(self._NumberOfNeurons/cs)
+        r = MPI.COMM_WORLD.rank
+        k1 = self.MLFlow(self._t, self._Xp)
+        k2 = self.MLFlow(self._t+self._dt/2, self._Xp+k1*self._dt/2)
+        k3 = self.MLFlow(self._t+self._dt/2, self._Xp+k2*self._dt/2)
+        k4 = self.MLFlow(self._t+self._dt  , self._Xp+k2*self._dt  )
+        self._Xp = self._Xp + (k1 + 2*k2 + 2*k3 + k4)*self._dt/6
         self._t = self._t + self._dt
+
+        self._Vp = self._Xp[:s]
+        self._Np = self._Xp[s:]
 
     def UpdateEuler(self,ii):
         #self._XX[ii,:] = self._X;
         #self._dXX[ii,:] = self._dX;
 
-        self._dX = self.Flow(self._t, self._X, self._params);
+        self._dX = self.MLFlow(self._t, self._X);
         self._X  = self._X + self._dt*self._dX;
         self._t  = self._t + self._dt
 
@@ -182,23 +221,49 @@ class NeuronModel():
         self._X[self._NumberOfNeurons:] += np.random.normal(self._NoiseMean, self._NoiseSTD, self._NumberOfNeurons)
 
     def Simulate(self, source='Jupyter'):
-        self._dX = np.array(self.MLFlow(self._t, self._X))
         self.Initialize()
+
+        self._dXp = np.array(self.MLFlow(self._t, self._Xp))
+
 
         if source=='Jupyter':
             for ii in range(len(self._Time)):
                 self.WriteData()
                 self.StoreTimeSeriesData(ii)
-                self.updateSynapses(ii)
+                self.UpdateSynapses(ii)
+                self.MPICOMM()
                 self.AddNoise(ii)
                 self.UpdateRK(ii);
+                if MPI.COMM_WORLD.rank == 0:
+                    print("step ",ii, " of ", len(self._Time))
         else:
             for ii in range(len(self._Time)):
                 self.WriteData()
                 self.StoreTimeSeriesData(ii)
-                self.updateSynapses(ii)
                 self.AddNoise(ii)
+                self.UpdateSynapses(ii)
                 self.UpdateRK(ii);
+                self.MPICOMM()
+
+
+    def MPICOMM(self):
+        MPI.COMM_WORLD.Barrier()
+        MPI.COMM_WORLD.Gather( [self._Vp, MPI.DOUBLE], [self._V, MPI.DOUBLE] )
+        MPI.COMM_WORLD.Gather( [self._Np, MPI.DOUBLE], [self._N, MPI.DOUBLE] )
+        MPI.COMM_WORLD.Gather( [self._dVp, MPI.DOUBLE], [self._dV, MPI.DOUBLE] )
+        MPI.COMM_WORLD.Gather( [self._dNp, MPI.DOUBLE], [self._dN, MPI.DOUBLE] )
+        #MPI.COMM_WORLD.Gather( [self._Inputp, MPI.DOUBLE], [self._Input, MPI.DOUBLE] )
+        self._X = np.concatenate((self._V,self._N))
+        self._dX = np.concatenate((self._dV,self._dN))
+        #self.SplitData()
+
 
     def WriteData(self):
-        self._Storage.WriteLine()
+        if MPI.COMM_WORLD.rank == 0:
+            self._Storage.WriteLine()
+
+
+if __name__=='__main__':
+    comm = MPI.COMM_WORLD
+    print("Hello! I'm rank %d from %d running in total..." % (comm.rank, comm.size))
+    comm.Barrier()   # wait for everybody to synchronize _here_

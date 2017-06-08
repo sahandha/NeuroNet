cd7618fce269d3cd51700e3eb6cd29c60c5add23
Sahand Hariri <sahand@Sahands-MacBook-Pro.local>
2017-06-08 12:31:59 -0500

    .

--- NeuronModel/NeuronModel.py
+++ NeuronModel/NeuronModel.py
@@ -161,7 +161,7 @@ class NeuronModel():
     def CreateCUDAKernel(self):
         initvalue = "0"
         mapper = '''
-        '''
+'''
             float w;
             float d;
 
@@ -171,12 +171,12 @@ class NeuronModel():
                 d = sqrt((X-x)^2 + (Y-y)^2);
                 w[i] = min(NetworkDevelTime*exp(-d/ConnectionScale)), SynapseLimit);
             }
-        '''
-        '''
+'''
+'''
         #1/SynapseLimit*w*CellType[i]*1/(1+exp(-input[i]));
         #reducer = "a+b"
         cudafunctionarguments = '''
-        '''
+'''
             float* input, float* X, float* Y, float* CellType, float* w,
             float Neuron,
             float NumberOfNeurons,
@@ -184,8 +184,8 @@ class NeuronModel():
             float ConnectionScale,
             float SynapseLimit,
             float x, float y"
-        '''
-        '''
+'''
+'''
         #kernel = reduction.ReductionKernel(np.float32, neutral = initvalue,
         #                                    reduce_expr=reducer, map_expr = mapper,
         #                                    arguments = cudafunctionarguments)

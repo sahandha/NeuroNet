31c4edee612261cc83e4181114c9f0d0ae894b55
Sahand Hariri <sahand@Sahands-MacBook-Pro.local>
2017-08-27 18:52:34 -0500

    Run bunchs of sims

---
+++ NeuronModel/runqsub.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+qsub jobscript0.pbs
+qsub jobscript1.pbs
+qsub jobscript2.pbs
+qsub jobscript3.pbs
+qsub jobscript4.pbs
+
+qsub jobscript5.pbs
+qsub jobscript6.pbs
+qsub jobscript7.pbs
+qsub jobscript8.pbs
+qsub jobscript9.pbs
+
+qsub jobscript10.pbs
+qsub jobscript11.pbs
+qsub jobscript12.pbs
+qsub jobscript13.pbs
+qsub jobscript14.pbs
